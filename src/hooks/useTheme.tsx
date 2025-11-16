624662320e44a8b0108f4c7189d8a7447c262b3c
Aabhas Sao <aabhassao0@gmail.com>
2025-11-16 17:38:49 +0530

    ci: fix pnpm build issues

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState, ReactNode } from 'react'
+import { createContext, useContext, useEffect, useState, type ReactNode } from 'react'
 
 type Theme = 'light' | 'dark'
 
